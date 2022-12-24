fb785b605254581bfa6e714d2632616704db8add
olegudz <olegpringudz@gmail.com>
2022-12-24 19:15:05 +0200

    new commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response('123');
 });
 
 Route::group(['namespace' => 'Main', 'prefix' => 'olegs'], function () {

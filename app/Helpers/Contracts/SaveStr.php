28e8668f590ee89617c9624f1fcde660b60840e4
“Oleg <olegpringudz@gmail.com>
2023-01-22 12:20:37 +0200

    up 7

--- app/Helpers/Contracts/SaveStr.php
+++ app/Helpers/Contracts/SaveStr.php
@@ -2,7 +2,11 @@
 
 namespace App\Helpers\Contracts;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+
 interface SaveStr
 {
-
+    public static function save(Request $request, User $user);
+    public function checkData($array);
 }
